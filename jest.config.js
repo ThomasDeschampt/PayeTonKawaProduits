ef4bf74e0eb7641d1a26f0c145c3fa70ffbd4906
YohDev24 <yohan.cas.pro@gmail.com>
2025-06-12 10:30:46 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,8 @@ module.exports = {
     forceExit: true,
     clearMocks: true,
     resetMocks: true,
-    restoreMocks: true,
+    restoreMocks: true
+        
   coverageThreshold: {
     global: {
       branches: 95,
